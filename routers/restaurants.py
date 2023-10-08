d9f6ad43845cd014a9cf8d4b7f1b364ab55370a3
Servando Valdez <96026888+Servando-Valdez@users.noreply.github.com>
2023-10-08 15:11:08 -0700

    fix routers

--- routers/restaurants.py
+++ routers/restaurants.py
@@ -1,15 +1,18 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, HTTPException, Request
 from schemas.restaurants import RestaurantRequest, RestaurantResponse
 from configs.database import get_db
-from services.restaurants import RestautantService
+from services.restaurants import RestaurantService
+from uuid import UUID
+
 router = APIRouter(
     prefix="/restaurants",
     tags=["restaurants"],
 )
 
+
 @router.get("/", response_model=list[RestaurantResponse])
 async def get_all_restaurants(db: get_db = Depends()):
-    restaurants = RestautantService(db).get_all()
+    restaurants = RestaurantService(db).get_all()
     for restaurant in restaurants:
         restaurant.__dict__.pop("_sa_instance_state")
     return restaurants
@@ -17,13 +20,21 @@ async def get_all_restaurants(db: get_db = Depends()):
 @router.post("/", status_code=201, response_model=RestaurantResponse)
 async def create_restaurant(item: RestaurantRequest, db: get_db = Depends()):
     item = item.__dict__
-    restaurant = RestautantService(db).create(item)
+    restaurant = RestaurantService(db).create(item)
     return restaurant
 
-@router.get("/{restaurant_id}")
-async def get_restaurant(restaurant_id: str):
-    return {"message": f"Get restaurant with id {restaurant_id}"}
+@router.get("/{restaurant_id}", status_code=200 ,response_model=RestaurantResponse)
+async def get_restaurant(restaurant_id: UUID, db: get_db = Depends()):
+    try: 
+        restaurant = RestaurantService(db).get_by_id(restaurant_id)
+        return restaurant
+    except Exception as e:
+        raise e
 
 @router.patch("/{restaurant_id}")
-async def update_restaurant(restaurant_id: str):
-    return {"message": f"Update restaurant with id {restaurant_id}"}
+async def update_restaurant(restaurant_id: UUID, db: get_db = Depends()):
+    try:
+        restaurant = RestaurantService(db).update(restaurant_id)
+        return restaurant
+    except Exception as e:
+        raise e
