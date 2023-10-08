0251d7a07e9f7fd8dbcccf3f8ec26e329864b1da
Servando Valdez <96026888+Servando-Valdez@users.noreply.github.com>
2023-10-08 15:10:30 -0700

    schemas restaurant update

--- schemas/restaurants.py
+++ schemas/restaurants.py
@@ -1,8 +1,12 @@
+from typing import Optional
 from pydantic import BaseModel, constr, conint, Field, UUID4
 from uuid import uuid4
 class UpdateModel(BaseModel):
     class Config:
         extra = "forbid"
+
+# class ErrorResponse(BaseModel):
+#     message: str
 
 class RestaurantRequest(UpdateModel):
     rating: conint(ge=0, le=4)
@@ -28,3 +32,15 @@ class RestaurantResponse(UpdateModel):
     state: str
     lat: float
     ing: float
+
+class UpdateRestaurantRequest(UpdateModel):
+    rating: Optional[conint(ge=0, le=4)]
+    name: Optional[str] 
+    site: Optional[str]
+    email: Optional[str]
+    phone: Optional[str]
+    street: Optional[str]
+    city: Optional[str]
+    state: Optional[str]
+    lat: Optional[float]
+    ing: Optional[float]
