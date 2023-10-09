9d9b0e24f3c2e7f80a46af7a0e3c57420760b5e2
Servando Valdez <96026888+Servando-Valdez@users.noreply.github.com>
2023-10-08 23:21:18 -0700

    create tables

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from configs.database import create_tables
 #routers
 from routers import restaurants
@@ -6,11 +7,23 @@ from models.restaurants import Restaurant
 
 app = FastAPI()
 
+# origins = [
+#     "http://localhost:8005/",
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-# create_tables()
+create_tables()
 # Base.metadata.create_all(bind=engine)
 
 app.include_router(restaurants.router)
